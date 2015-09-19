473b034302ead4933f18d7dc7d25cc3f81bb0cf6
camilo9020 <camilo9020@gmail.com>
2015-09-19 07:23:29 -0500

    Add answers controller and column votes to questions model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150918202307) do
+ActiveRecord::Schema.define(version: 20150919121822) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "user_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150918202307) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "description"
+    t.integer  "votes"
   end
 
 end
