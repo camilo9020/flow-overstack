5267430a4aebc17074778d2d916cc690445969b8
Juan Camilo Hernández <camilo9020@gmail.com>
2018-04-11 20:04:30 -0500

    install annotate

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: answers
+#
+#  id          :integer          not null, primary key
+#  question_id :integer
+#  body        :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
 class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
